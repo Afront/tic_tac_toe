75277dc468d5b10267f777c6d2ca9264b27dc7f6
Afront <3943720+Afront@users.noreply.github.com>
2020-02-22 23:51:16 +0900

    Add some game mechanics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use promptly::prompt;
 
+#[derive(Debug, PartialEq)]
+enum State {
+	Blank,
+	FirstPlayer,
+	SecondPlayer,
+}
+
+impl State {
+	fn as_uint16(&self) -> u16 {
+		match self {
+			State::FirstPlayer => 1,
+			State::SecondPlayer => 2,
+			State::Blank => 0,
+		}
+	}
+
+	fn next(&self) -> Self {
+		match self {
+			State::FirstPlayer => State::SecondPlayer,
+			State::SecondPlayer => State::FirstPlayer,
+			State::Blank => State::Blank,
+		}
+	}
+}
+
 /**
  * Possibilities for grid:
  * A 2D array: size = 9
@@ -20,17 +45,49 @@ impl Default for TicTacToe {
 }
 
 impl TicTacToe {
-	fn print(self) {
+	fn print(&self) {
 		let symbols = ["_", "O", "X"];
-		let mut state = 19683;
-		let mut i: i32 = 0;
-		while state != 1 {
-			print!("{} ", symbols[state % 3]);
+		let mut state = self.grid as usize;
+		let mut i: u8 = 1;
+		while i != 10 {
+			let mut square = symbols[state % 3].to_owned();
+			if square == "_" {
+				square = i.to_string();
+			}
+			print!("{} ", square);
 			state /= 3;
-			i += 1;
 			if (i % 3) == 0 {
 				println!();
 			}
+			i += 1;
+		}
+	}
+
+	fn _solved(&self) -> bool {
+		if self.grid == 3 {}
+		true
+	}
+
+	fn choose_spot(&mut self, spot: u16, turn: &State) {
+		if spot != 0 {
+			self.grid += turn.as_uint16() + 3_u16.pow((9 - spot).into());
+		} else {
+			self.grid += turn.as_uint16();
+		}
+	}
+
+	fn get_spot_owner(&self, spot: u16) -> State {
+		let mut i = 0;
+		let mut grid = self.grid;
+		while i < spot {
+			grid /= 3;
+			i += 1;
+		}
+		match grid % 3 {
+			0 => State::Blank,
+			1 => State::FirstPlayer,
+			2 => State::SecondPlayer,
+			_ => State::Blank,
 		}
 	}
 }
@@ -42,22 +99,48 @@ fn abort() {
 fn help() {
 	println!("(H)elp  => Prints this help screen");
 	println!("(Q)uit | Abort | Exit  => Exit the app");
-	println!("Sign (U)p | Register | Signup => Sign up to the app");
-	println!("Sign (I)n | Signin | Login | Log in | Log on => Sign in to the app");
 }
 
 fn computer() {
 	play()
 }
 
-fn friend() {}
+fn spot_taken(handler: &TicTacToe, spot: u16) -> bool {
+	handler.get_spot_owner(spot) != State::Blank
+}
+
+fn friend() {
+	let mut turn = State::FirstPlayer;
+	let mut handler = TicTacToe::default();
+
+	loop {
+		println!(
+			"Player {:?}'s turn!",
+			if turn == State::FirstPlayer { 1 } else { 2 }
+		);
+
+		handler.print();
+
+		let number: String = prompt("Choose a number!");
+		let chosen_number: u16 = match number.parse() {
+			Ok(n) => n,
+			Err(_) => continue,
+		};
+		if !spot_taken(&handler, chosen_number) {
+			handler.choose_spot(chosen_number, &turn);
+		} else {
+			println!(
+				"There is already something occupying that spot! Please enter another number!"
+			);
+			continue;
+		}
+		turn = turn.next();
+	}
+}
 
 fn online() {}
 
-fn play() {
-	let handler = TicTacToe::default();
-	handler.print();
-}
+fn play() {}
 
 fn main() {
 	loop {
@@ -72,5 +155,6 @@ fn main() {
 			"ONLINE" | "O" => online(),
 			_ => continue,
 		}
+		println!("Hi!");
 	}
 }
